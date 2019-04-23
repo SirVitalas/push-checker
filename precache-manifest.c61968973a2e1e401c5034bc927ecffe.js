804e2f942600e58480e4a693a00eda39908da041
Vitalii Ihnatov <ivitalas@gmail.com>
2019-04-23 14:21:02 +0300

    Updates

---
+++ precache-manifest.c61968973a2e1e401c5034bc927ecffe.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "47c4105e98e30abf6f74",
+    "url": "/SirVitalas/push-checker/static/js/runtime~main.94b6ed35.js"
+  },
+  {
+    "revision": "177e20a9d1afaea4d0a3",
+    "url": "/SirVitalas/push-checker/static/js/main.8ef4cc9a.chunk.js"
+  },
+  {
+    "revision": "7515c37ed2666477a566",
+    "url": "/SirVitalas/push-checker/static/js/2.f0d34e61.chunk.js"
+  },
+  {
+    "revision": "177e20a9d1afaea4d0a3",
+    "url": "/SirVitalas/push-checker/static/css/main.2bf9ce4d.chunk.css"
+  },
+  {
+    "revision": "fb961c1fa23e59740ad2383796d21753",
+    "url": "/SirVitalas/push-checker/index.html"
+  }
+];
