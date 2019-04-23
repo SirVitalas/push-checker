736243b98ae9e2512e85aa798b53719a9a0ca6bf
Vitalii Ihnatov <ivitalas@gmail.com>
2019-04-23 15:02:53 +0300

    Updates

---
+++ precache-manifest.791aafeb8f1ba85eb9d87277c27216aa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "95c41b890d8173a3d603",
+    "url": "/push-checker/static/js/runtime~main.9512339a.js"
+  },
+  {
+    "revision": "177e20a9d1afaea4d0a3",
+    "url": "/push-checker/static/js/main.8ef4cc9a.chunk.js"
+  },
+  {
+    "revision": "7515c37ed2666477a566",
+    "url": "/push-checker/static/js/2.f0d34e61.chunk.js"
+  },
+  {
+    "revision": "177e20a9d1afaea4d0a3",
+    "url": "/push-checker/static/css/main.2bf9ce4d.chunk.css"
+  },
+  {
+    "revision": "344e08ae7415d411d0207460861695da",
+    "url": "/push-checker/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/SirVitalas/push-checker/precache-manifest.ffa569224835fb51b2e836e6be229936.js"
+  "/push-checker/precache-manifest.791aafeb8f1ba85eb9d87277c27216aa.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/SirVitalas/push-checker/index.html", {
+workbox.routing.registerNavigationRoute("/push-checker/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
